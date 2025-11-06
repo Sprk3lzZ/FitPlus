55dc983ab03a6043ddec9c019a2850d6bb1e2dfa
Sprk3lzZ <Yanis.zge@icloud.com>
2025-11-05 17:22:44 -0800

    feat: add project base

---
+++ FitPlus/FitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPlusApp.swift
+//  FitPlus
+//
+//  Created by Yanis Zeghiche on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
